cfaafec4db4cd6419d103e4bc8114c49e5a755e0
Alexandre Sartor <xandesartorteixeira@gmail.com>
2024-12-06 00:04:28 -0300

    Organizando o projeto

--- atualizaçao/src/components/Pages/Compras/Compras.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Compras.css";
-
-const Compras = () => {
-  return <div></div>;
-};
-import "./Compras.css";
-
-export default Compras;
--- atualizaçao/src/components/Pages/Infos/Infos.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Infos.css";
-
-const Infos = () => {
-  return <div></div>;
-};
-import "./Infos.css";
-
-export default Infos;
